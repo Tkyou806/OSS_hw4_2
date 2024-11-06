43fa1769dad353ff6850332d5777c987202f0e17
Tkyou <stoa806@handong.ac.kr>
2024-11-07 00:09:07 +0900

    new start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from'./components/Hello.js';
+import Content from'./components/Content.js';
 
 function App() {
   return (
@@ -7,7 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Hello />
+          <Content/>
         </p>
         <a
           className="App-link"
