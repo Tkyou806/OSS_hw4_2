9fe6a53c73ae0ce26a9b9c0c97a40a879bb6e1cb
Tkyou <stoa806@handong.ac.kr>
2024-11-06 21:42:30 +0900

    Finish react!

---
+++ newproject/src/components/Content.js
@@ -0,0 +1,20 @@
+import './Hello.css';
+
+function content(props) {
+    return (
+        <>
+
+            <p>If you join us, Click this!</p>
+            <a
+                className="App-link"
+                href="https://hisnet.handong.edu/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                hisnet
+            </a>
+        </>
+    );
+}
+
+export default content;
---
+++ newproject/src/components/Hello.js
@@ -0,0 +1,12 @@
+import './Hello.css';
+
+function Hello(props) {
+    return (
+        <>
+            <h1>2024 OSS </h1>
+            <h3>2024-2 Handong University CSS</h3>
+        </>
+    );
+}
+
+export default Hello;
